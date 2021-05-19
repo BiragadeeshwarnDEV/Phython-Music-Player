6e706dd8095fac4baa5e534d104dc3c28413b329
BiragadeeshwarnDEV <84442817+BiragadeeshwarnDEV@users.noreply.github.com>
2021-05-19 14:32:37 +0530

    Update and rename hdh.py to MusicPlayer.py

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -3,5 +3,5 @@ import time
 print("Loading Audio..........")
 time.sleep(3)
 print("Playing Now")
-ps.playsound('Hello.mp3')
+ps.playsound('filename.mp3')
 # Move Your Audio On The Folder To Play It
